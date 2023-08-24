9e097785e82ac08f6efa673454e16751695c0a0d
DhanashriRekhate29 <143004797+DhanashriRekhate29@users.noreply.github.com>
2023-08-24 18:25:18 +0530

    Update A.java

--- StartToEnd/src/Scanner/A.java
+++ StartToEnd/src/Scanner/A.java
@@ -6,10 +6,10 @@ public static void main(String args[])
 {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter first number");
-	int x=sc.nextInt();
+	int a=sc.nextInt();
 	System.out.println("Enter second number");
 	int y=sc.nextInt();
-	int z=x+y;
+	int z=a+y;
 			System.out.println(z);
 }
 }
